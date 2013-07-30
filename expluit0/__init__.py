ad81c55189175d453dc156c35e7ee8e475e071a3
Posquit0 <posquit0.bj@gmail.com>
2013-07-31 02:20:51 +0900

    add contents to __init__.py

---
+++ expluit0/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This package provides an exploit framework for your CTF.
+
+Usage
+=====
+
+To be updated at the future.
+"""
+
+__authors__   = "Posquit0.BJ"
+__license__   = "MIT"
+__docformat__ = "restructuredtext en"
